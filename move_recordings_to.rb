c3004e085d7f4d734fa289272444da2f847fd95a
Lito Manansala <avmanansala@gmail.com>
2010-02-19 18:21:22 +0800

    Added date format comments

--- move_recordings_to.rb
+++ move_recordings_to.rb
@@ -2,7 +2,7 @@
 #Move recording files to a seperate DATE directory, this is usefull if your directory is growing fast.
 #Copyright 2010 Lito Manansala avmanansala@gmail.com License GPL v2
 #sample usage: ./move_recordings_to.irb 20100219
-#Date Sample format 20100219
+#Date Sample format 20100219 for Feb 19, 2010
 
 require 'ftools'
 dateq = ARGV[0]
