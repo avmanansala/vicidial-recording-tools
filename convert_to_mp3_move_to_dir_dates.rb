5f0d5af2f106c5611670139504cd781e0e0cd54b
Lito Manansala <avmanansala@gmail.com>
2010-02-22 20:15:42 +0800

    added rubygems require

--- convert_to_mp3_move_to_dir_dates.rb
+++ convert_to_mp3_move_to_dir_dates.rb
@@ -1,4 +1,22 @@
 #!/usr/bin/env ruby
-require 'conn_db.rb'
+#Copyright 2010 Lito Manansala avmanansala@gmail.com License GPL v2
+#
+#
+#
+require 'rubygems'
+require 'db_conn.rb'
+require 'active_support'
 
-puts "hello worlds"
+num_of_days = 130
+base_dir = "/var/spool/asterisk/monitor/ORIG"
+dest_dir = "/var/spool/asterisk/monitor/MP3"
+
+puts "Creating Directories..."
+for i in 1..num_of_days
+  dir_to_create = i.days.ago.strftime("%b-%d-%Y")
+  puts "Creating  \"#{dest_dir}/#{dir_to_create}\" directory"
+  exec "mkdir #{dest_dir}/#{dir_to_create}"
+end
+puts "done creating directories..."
+
+# Listing all -all.wav files
--- db_conn.rb
+++ db_conn.rb
@@ -1,3 +1,6 @@
+#
+##Copyright 2010 Lito Manansala avmanansala@gmail.com License GPL v2
+#
 require ('rubygems')
 require ('parseconfig')
 require ('active_record')
@@ -11,5 +14,7 @@ my_config = ParseConfig.new('/etc/astguiclient.conf')
 @mysql_user = my_config.get_value('VARDB_user').gsub('>','').strip
 @mysql_pass = my_config.get_value('VARDB_pass').gsub('>','').strip
 @mysql_port = my_config.get_value('VARDB_port').gsub('>','').strip
+@twiter_username = ""
+@twitter_password = ""
 #end::Parse config
 
